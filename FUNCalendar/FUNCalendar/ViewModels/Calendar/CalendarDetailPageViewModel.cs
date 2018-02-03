22034141f620759fd99e807f5abd9621550a450f
thomson-san <Thomsoon2017@gmail.com>
2018-02-03 19:37:38 +0900

    カレンダー詳細ページでToDoの完了、WishListの購入済みが行えるように修正

--- FUNCalendar/FUNCalendar/ViewModels/Calendar/CalendarDetailPageViewModel.cs
+++ FUNCalendar/FUNCalendar/ViewModels/Calendar/CalendarDetailPageViewModel.cs
@@ -62,6 +62,12 @@ namespace FUNCalendar.ViewModels
         private bool todoListOpen = false;
         private bool householdAccountsListOpen = false;
 
+        /* 完了コマンド */
+        public ReactiveCommand CompleteCommand { get; private set; } = new ReactiveCommand();
+
+        /* 購入済みコマンド */
+        public AsyncReactiveCommand BoughtCommand { get; private set; } = new AsyncReactiveCommand();
+
         /* 廃棄 */
         private CompositeDisposable Disposable { get; } = new CompositeDisposable();
 
@@ -261,6 +267,167 @@ namespace FUNCalendar.ViewModels
                         navigationParameters.Add(HouseholdAccountsRegisterPageViewModel.CalendarKey, navigationitem);
                         await this._navigationService.NavigateAsync($"/NavigationPage/HouseholdAccountsRegisterPage", navigationParameters);
                         break;
+                }
+            });
+
+            /* 完了ボタンが押されたときの処理 */
+            CompleteCommand.Subscribe(async (obj) =>
+            {
+                var todoitem = VMToDoItem.ToToDoItem((obj as VMToDoItem));
+                /* 完了済みの場合 */
+                if (todoitem.IsCompleted)
+                {
+                    await _pageDialogService.DisplayAlertAsync("注意", "このアイテムは既に完了済みです。", "確認");
+                }
+                /* 完了済みでない場合 */
+                else
+                {
+                    /* wishIDを持っている場合 */
+                    if ((obj as VMToDoItem).WishID != 0)/* wishIDを持っているか */
+                    {
+                        /* ダイアログで家計簿に登録するか確認 */
+                        var result = await _pageDialogService.DisplayAlertAsync("確認", "このアイテムを家計簿に登録しますか？", "はい", "いいえ");
+
+                        /* ダイアログの確認で家計簿に登録するを選択した場合 */
+                        if (result)
+                        {
+                            string scategory, dcategory, storagetype;
+                            scategory = await _pageDialogService.DisplayActionSheetAsync("概要カテゴリーの設定", null, null, "食費", "日用雑貨", "交通費", "娯楽費", "医療費", "通信費", "水道_光熱費", "その他_支出");
+                            switch (scategory)
+                            {
+                                case "食費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "食料品", "朝食", "昼食", "夕食", "その他_食費");
+                                    break;
+                                case "日用雑貨":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "消耗品", "子供関連", "ペット関連", "その他_日用雑貨");
+                                    break;
+                                case "交通費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "電車", "タクシー", "バス", "飛行機", "その他_交通費");
+                                    break;
+                                case "娯楽費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "レジャー", "イベント", "映画", "音楽", "漫画", "書籍", "ゲーム", "その他_娯楽費");
+                                    break;
+                                case "医療費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "病院代", "薬代", "生命保険", "医療保険", "その他_医療費");
+                                    break;
+                                case "通信費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "携帯電話料金", "固定電話料金", "インターネット関連", "テレビ受信料", "宅配便", "切手_はがき", "その他_通信費");
+                                    break;
+                                case "水道_光熱費":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "水道料金", "電気料金", "ガス料金", "その他_水道_光熱費");
+                                    break;
+                                case "その他_支出":
+                                    dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "仕送り", "お小遣い", "使途不明金", "立替金", "その他_支出");
+                                    break;
+                                default:
+                                    dcategory = "食料品"; //とりあえず代入
+                                    break;
+                            }
+                            storagetype = await _pageDialogService.DisplayActionSheetAsync("出金元の設定", null, null, "財布", "貯金", "銀行", "クレジットカード", "その他");
+                            if (scategory == null || storagetype == null)
+                                return;
+                            var Scategory = (SCategorys)Enum.Parse(typeof(SCategorys), scategory);
+                            var Dcategory = (DCategorys)Enum.Parse(typeof(DCategorys), dcategory);
+                            var Storagetype = (StorageTypes)Enum.Parse(typeof(StorageTypes), storagetype);
+                            await _storageService.CompleteToDo(todoitem, true, result, Scategory, Dcategory, Storagetype);
+
+                        }
+                        /* 家計簿に登録しない場合 */
+                        else
+                        {
+                            /* とりあえず代入 */
+                            var Scategory = SCategorys.start_of_支出;
+                            var Dcategory = DCategorys.start_of_その他_支出;
+                            var Storagetype = StorageTypes.start_of_Stype;
+                            await _storageService.CompleteToDo(todoitem, true, false, Scategory, Dcategory, Storagetype);
+                        }
+                    }
+                    /* wishIDを持っていない場合 */
+                    else
+                    {
+                        /* とりあえず代入 */
+                        var Scategory = SCategorys.start_of_支出;
+                        var Dcategory = DCategorys.start_of_その他_支出;
+                        var Storagetype = StorageTypes.start_of_Stype;
+                        await _storageService.CompleteToDo(todoitem, false, false, Scategory, Dcategory, Storagetype);
+                    }
+                }
+            });
+
+
+            /*購入完了ボタンが押されたときの処理 */
+            BoughtCommand.Subscribe(async (obj) =>
+            {
+                var wishitem = VMWishItem.ToWishItem((obj as VMWishItem));
+                /* 購入済みの場合 */
+                if (wishitem.IsBought)
+                {
+                    await _pageDialogService.DisplayAlertAsync("注意", "このアイテムは既に購入済みです", "確認");
+                }
+                /* 購入済みでなかった時 */
+                else
+                {
+                    var hasID = false;
+                    /* todoidを持っていたら */
+                    if ((obj as VMWishItem).ToDoID != 0)
+                    {
+                        hasID = true;
+                    }
+
+                    /* ダイアログで家計簿に登録するか確認 */
+                    var result = await _pageDialogService.DisplayAlertAsync("確認", "このアイテムを家計簿に登録しますか？", "はい", "いいえ");
+
+                    /* ダイアログの確認で家計簿に登録するを選択した場合 */
+                    if (result)
+                    {
+                        string scategory, dcategory, storagetype;
+                        scategory = await _pageDialogService.DisplayActionSheetAsync("概要カテゴリーの設定", null, null, "食費", "日用雑貨", "交通費", "娯楽費", "医療費", "通信費", "水道_光熱費", "その他_支出");
+                        switch (scategory)
+                        {
+                            case "食費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "食料品", "朝食", "昼食", "夕食", "その他_食費");
+                                break;
+                            case "日用雑貨":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "消耗品", "子供関連", "ペット関連", "その他_日用雑貨");
+                                break;
+                            case "交通費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "電車", "タクシー", "バス", "飛行機", "その他_交通費");
+                                break;
+                            case "娯楽費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "レジャー", "イベント", "映画", "音楽", "漫画", "書籍", "ゲーム", "その他_娯楽費");
+                                break;
+                            case "医療費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "病院代", "薬代", "生命保険", "医療保険", "その他_医療費");
+                                break;
+                            case "通信費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "携帯電話料金", "固定電話料金", "インターネット関連", "テレビ受信料", "宅配便", "切手_はがき", "その他_通信費");
+                                break;
+                            case "水道_光熱費":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "水道料金", "電気料金", "ガス料金", "その他_水道_光熱費");
+                                break;
+                            case "その他_支出":
+                                dcategory = await _pageDialogService.DisplayActionSheetAsync("詳細カテゴリーの設定", null, null, "仕送り", "お小遣い", "使途不明金", "立替金", "その他_支出");
+                                break;
+                            default:
+                                dcategory = "食料品"; //とりあえず代入
+                                break;
+                        }
+                        storagetype = await _pageDialogService.DisplayActionSheetAsync("出金元の設定", null, null, "財布", "貯金", "銀行", "クレジットカード", "その他");
+
+                        var Scategory = (SCategorys)Enum.Parse(typeof(SCategorys), scategory);
+                        var Dcategory = (DCategorys)Enum.Parse(typeof(DCategorys), dcategory);
+                        var Storagetype = (StorageTypes)Enum.Parse(typeof(StorageTypes), storagetype);
+                        await _storageService.BoughtWishItem(wishitem, hasID, true, Scategory, Dcategory, Storagetype);
+                    }
+                    /* 家計簿に登録しない場合 */
+                    else
+                    {
+                        /* とりあえず代入 */
+                        var Scategory = SCategorys.start_of_支出;
+                        var Dcategory = DCategorys.start_of_その他_支出;
+                        var Storagetype = StorageTypes.start_of_Stype;
+                        await _storageService.BoughtWishItem(wishitem, hasID, false, Scategory, Dcategory, Storagetype);
+                    }
                 }
             });
         }
